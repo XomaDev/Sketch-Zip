9aa64d09274b7f09884770506a0755f008873465
kumaraswamy <care.xoma@gmail.com>
2023-03-03 18:12:20 +0530

    Remove unnecessary fields

--- src/xyz/kumaraswamy/sketchzip/structures/Block.java
+++ src/xyz/kumaraswamy/sketchzip/structures/Block.java
@@ -5,21 +5,15 @@ public final class Block {
   public final SketchList list;
 
   public final byte dictPoint;
-  public final int onset;
-  public final int offset;
 
-  public Block(byte[] block, byte dictPoint, int onset, int offset) {
+  public Block(byte[] block, byte dictPoint) {
     this.list = new SketchList(block);
     this.dictPoint = dictPoint;
-    this.onset = onset;
-    this.offset = offset;
   }
 
   @Override
   public String toString() {
     return "Block[" +
-            "block=" + list + ", " +
-            "onset=" + onset + ", " +
-            "offset=" + offset + ']';
+            "block=" + list + "]";
   }
 }
